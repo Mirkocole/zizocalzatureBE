c27e4f5a687721386d3d6c4606abef0c4bb86136
Mirkocole <mirkocole92@gmail.com>
2025-01-09 12:53:48 +0100

    email sender

--- middleware/mail.js
+++ middleware/mail.js
@@ -8,20 +8,30 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // true for port 465, false for other ports
     auth: {
-        user: "steviecolella@gmail.com",
-        pass: "zuim yjcv dqrs burj",
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
 });
 
  async function emailSend  (req, res, next) {
     try {
-        
+        // console.log(req.body)
         let mailSend = await transporter.sendMail({
-            from: '"Zizo Calzature website 💻" <mirkocole92@gmail.com>', // sender address
-            to: req.body.Email, // list of receivers
-            subject: `Messagio da ${req.body.Name} ${req.body.Lastname}`, // Subject line
-            text: req.body.Message, // plain text body
-            html: `<b>${req.body.Message}</b>`, // html body
+            sender: req.body.email,
+            replyTo: req.body.email,
+            from: `${req.body.email} "Zizo Calzature website 💻"`, // sender address
+            to: process.env.MAIL_USER, // list of receivers
+            subject: `Messagio da ${req.body.nome} ${req.body.cognome}`, // Subject line
+            text: req.body.messaggio, // plain text body
+            html: `<p>
+            Nome: ${req.body.nome} <br />
+            Cognome: ${req.body.cognome} <br />
+            Email: ${req.body.email} <br />
+            Telefono: ${req.body.telefono} <br />
+            Messaggio:  
+            ${req.body.messaggio}
+            
+            </p>`, // html body
             
         })
         
